f48ce70fb4040ffa3906a9f0d1b0fdbe4d803e09
nekus8814 <ovira@inbox.lv>
2025-06-23 10:27:28 +0300

    Instance2

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "instancia" {
   ami = "ami-0fdbea22f3180a1ca"
   instance_type = "t3.micro"
+}
 
 resource "aws_s3_bucket" "vedr0" {
   bucket = "bucket-nekus8814"
