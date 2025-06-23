4b485741a15e5ea18e0857a2eeff958edadaf8c5
nekus8814 <ovira@inbox.lv>
2025-06-23 10:17:42 +0300

    Var changes

--- variables.tf
+++ variables.tf
@@ -1,12 +1,6 @@
 # variables.tf
-#variable "region" {}
-#variable "bucket_name" {}
+variable "region" {}
+variable "bucket_name" {}
 
-provider "aws" {
-  region = var.region
-}
 
-resource "aws_s3_bucket" "bucket" {
-  bucket = var.bucket_name
-}
 
