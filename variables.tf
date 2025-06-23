3b52d927873a23bbdb6791e75bebf6bdef8ed1a3
nekus8814 <ovira@inbox.lv>
2025-06-23 10:12:45 +0300

    da

--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
 # variables.tf
-#variable "region" {}
-#variable "bucket_name" {}
+variable "region" {}
+variable "bucket_name" {}
 
-#provider "aws" {
-#  region = var.region
-#}
+provider "aws" {
+  region = var.region
+}
 
-#resource "aws_s3_bucket" "bucket" {
-#  bucket = var.bucket_name
-#}
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.bucket_name
+}
 
